a986c1a6898fc1ec4a33d92c108bb54c82947041
Valoeghese <valoeghesemods@gmail.com>
2022-03-02 00:23:35 +1300

    Implement a Jittered Grid for voronoi

---
+++ JitteredGrid.cpp
@@ -0,0 +1,149 @@
+// Based off of code I wrote here https://github.com/valoeghese/2fc0f18/blob/master/src/main/java/tk/valoeghese/fc0/world/kingdom/Voronoi.java
+
+#include "JitteredGrid.h"
+
+const int64_t kMaxInt64 = std::numeric_limits<int64_t>::max();
+
+// constructors
+
+cubewg::JitteredGrid::JitteredGrid() {
+	this->seed = 0;
+	this->relaxation = 0;
+}
+
+cubewg::JitteredGrid::JitteredGrid(const int64_t seed) {
+	this->seed = seed;
+	this->relaxation = 0;
+}
+
+cubewg::JitteredGrid::JitteredGrid(const int64_t seed, const double relaxation) {
+	this->seed = seed;
+	this->relaxation = relaxation;
+}
+
+// methods
+
+int64_t Random(int64_t seed, int64_t x, int64_t y) {
+	// constants from MMIX, however the exact rng implementation differs
+	seed *= seed * 6364136223846793005 + 1442695040888963407;
+	seed += x;
+	seed *= seed * 6364136223846793005 + 1442695040888963407;
+	seed += y;
+	seed *= seed * 6364136223846793005 + 1442695040888963407;
+	seed += x;
+	seed *= seed * 6364136223846793005 + 1442695040888963407;
+	seed += y;
+	return seed;
+}
+
+double RandomDouble(int64_t seed, int64_t x, int64_t y) {
+	return (double)Random(seed, x, y) / (double)kMaxInt64;
+}
+
+double SqrDist(double x0, double y0, double x1, double y1) {
+	double dx = x1 - x0;
+	double dy = y1 - y1;
+	return dx * dx + dy * dy;
+}
+
+cubewg::JitteredPoint cubewg::JitteredGrid::SampleGrid(int64_t grid_x, int64_t grid_y) {
+	double unrelaxation = 1.0 - this->relaxation; // the "opposite" of the relaxation in weighting the values
+	return cubewg::JitteredPoint(
+		this->relaxation * 0.5 + unrelaxation * RandomDouble(this->seed, grid_x, grid_y),
+		this->relaxation * 0.5 + unrelaxation * RandomDouble(this->seed + 1, grid_x, grid_y),
+		Random(this->seed + 2, grid_x, grid_y));
+}
+
+cubewg::JitteredPoint cubewg::JitteredGrid::SampleVoronoi(double x, double y) {
+	double unrelaxation = 1.0 - this->relaxation;
+
+	// coordinates of the grid area in the centre of the search. I.e. the grid area the point is actually in.
+	const uint64_t cgrid_x = (uint64_t)std::floor(x);
+	const uint64_t cgrid_y = (uint64_t)std::floor(y);
+
+	double result_x = 0;
+	double result_y = 0;
+	uint64_t result_grid_x = 0;
+	uint64_t result_grid_y = 0;
+	double result_dist = 1000.0; // 1000 is an insanely high value we can't possibly get, so it's used as a placeholder
+
+	for (int xo = -1; xo <= 1; xo++) {
+		int grid_x = cgrid_x + xo;
+
+		for (int yo = -1; yo <= 1; yo++) {
+			int grid_y = cgrid_y + yo;
+
+			double point_x = grid_x + this->relaxation * 0.5 + unrelaxation * RandomDouble(grid_x, grid_y, this->seed);
+			double point_y = grid_y + this->relaxation * 0.5 + unrelaxation * RandomDouble(grid_x, grid_y, this->seed + 1);
+			double point_dist = SqrDist(x, y, point_x, point_y);
+
+			if (point_dist < result_dist) {
+				result_x = point_x;
+				result_y = point_y;
+				result_grid_x = grid_x;
+				result_grid_y = grid_y;
+				result_dist = point_dist;
+			}
+		}
+	}
+
+	return JitteredPoint(result_x, result_y, Random(this->seed + 2, result_grid_x, result_grid_y));
+}
+
+double cubewg::JitteredGrid::Worley(double x, double y) {
+	double unrelaxation = 1.0 - this->relaxation;
+
+	// coordinates of the grid area in the centre of the search. I.e. the grid area the point is actually in.
+	const uint64_t cgrid_x = (uint64_t)std::floor(x);
+	const uint64_t cgrid_y = (uint64_t)std::floor(y);
+
+	double result_dist = 1000.0; // 1000 is an insanely high value we can't possibly get, so it's used as a placeholder
+
+	for (int xo = -1; xo <= 1; xo++) {
+		int grid_x = cgrid_x + xo;
+
+		for (int yo = -1; yo <= 1; yo++) {
+			int grid_y = cgrid_y + yo;
+
+			double point_x = grid_x + this->relaxation * 0.5 + unrelaxation * RandomDouble(grid_x, grid_y, this->seed);
+			double point_y = grid_y + this->relaxation * 0.5 + unrelaxation * RandomDouble(grid_x, grid_y, this->seed + 1);
+			double point_dist = SqrDist(x, y, point_x, point_y);
+
+			if (point_dist < result_dist) {
+				result_dist = point_dist;
+			}
+		}
+	}
+
+	return result_dist;
+}
+
+double cubewg::JitteredGrid::Worley2(double x, double y) {
+	double unrelaxation = 1.0 - this->relaxation;
+
+	// coordinates of the grid area in the centre of the search. I.e. the grid area the point is actually in.
+	const uint64_t cgrid_x = (uint64_t)std::floor(x);
+	const uint64_t cgrid_y = (uint64_t)std::floor(y);
+
+	double result_dist = 1000.0; // 1000 is an insanely high value we can't possibly get, so it's used as a placeholder
+	double result_dist_2 = 1000.0; // Again, a placeholder high value. This will be the second closest distance.
+
+	for (int xo = -1; xo <= 1; xo++) {
+		int grid_x = cgrid_x + xo;
+
+		for (int yo = -1; yo <= 1; yo++) {
+			int grid_y = cgrid_y + yo;
+
+			double point_x = grid_x + this->relaxation * 0.5 + unrelaxation * RandomDouble(grid_x, grid_y, this->seed);
+			double point_y = grid_y + this->relaxation * 0.5 + unrelaxation * RandomDouble(grid_x, grid_y, this->seed + 1);
+			double point_dist = SqrDist(x, y, point_x, point_y);
+
+			if (point_dist < result_dist) {
+				result_dist_2 = result_dist;
+				result_dist = point_dist;
+			}
+		}
+	}
+
+	return result_dist - result_dist_2;
+}
---
+++ JitteredGrid.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+#include <cmath>
+
+using std::int64_t;
+
+namespace cubewg {
+	struct JitteredPoint {
+		// the x position of this point
+		double x;
+		// this y position of this point
+		double y;
+		// an additional random number associated with this point
+		int64_t data;
+
+		JitteredPoint(double x, double y, int64_t data) {
+			this->x = x;
+			this->y = y;
+			this->data = data;
+		}
+	};
+
+	class JitteredGrid {
+	private:
+		int64_t seed;
+		double relaxation;
+	public:
+		JitteredGrid();
+		JitteredGrid(const int64_t seed);
+		JitteredGrid(const int64_t seed, const double relaxation);
+
+		/* Samples the point on the jittered grid at the given coordinates. The jittered point is guaranteed to fall within this grid space.
+		*/
+		JitteredPoint SampleGrid(int64_t grid_x, int64_t grid_y);
+
+		/* Samples the nearest jittered point to the given coordinates.
+		*/
+		JitteredPoint SampleVoronoi(double x, double y);
+
+		/* Samples worley noise using the points on this jittered grid. Uses euclidean squared distance.
+		*/
+		double Worley(double x, double y);
+
+		/* Samples 'd1-d2' type worley noise using the points on this jittered grid. Uses euclidean squared distance.
+		*/
+		double Worley2(double x, double y);
+	};
+}
