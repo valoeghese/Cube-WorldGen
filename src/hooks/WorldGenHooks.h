bbddd6c9428fdeacd674a74dfdcd3020daec8330
valoeghese <46117728+valoeghese@users.noreply.github.com>
2025-04-24 16:38:36 +1200

    feat: Winter Wonderland

--- src/hooks/WorldGenHooks.h
+++ src/hooks/WorldGenHooks.h
@@ -24,37 +24,20 @@ __attribute__((naked)) void ASMNop() {
 	);
 }*/
 
-extern "C"  cube::BlockProperties<float>*  __fastcall NewCalculateTopBlock(
-    cube::World* self,
-    cube::BlockProperties<float>* block_properties,
-    signed int x,
-    signed int y,
-    float z_coord,
-    float field_field_8,
-    float height_structure_west,
-    float height_structure_east,
-    float height_structure_north,
-    float height_structure_south,
-    float field_field_14,
-    float field_field_C,
-    float field_field_10,
-    float field_field_28,
-    float zero_maybe,
-    float field_field_38,
-    int field_field_18,
-    int field_field_1C,
-    int field_field_20,
-    int zero_maybe_1,
-    int zero_maybe_2)
+extern "C" float  __fastcall NewOverwriteWorldgen(
+	cube::World *world,
+	int x,
+	int y,
+	float density_maybe,
+	float west,
+	float east,
+	float north,
+	float south)
 {
-    block_properties->blue = 25;
-    block_properties->red = 150;
-    block_properties->green = 70;
-    block_properties->type = (cube::BlockProperties<float>::Type)1;
-    return block_properties;
+	return 1.0;
 }
 
 void SetupOverwriteWorldgen() {
 	char* base = (char*)CWBase();
-	WriteFarJMP(base + 0x2D8200, (void*)NewCalculateTopBlock);
+	WriteFarJMP(base + 0x2B4320, (void*)NewOverwriteWorldgen);
 }
